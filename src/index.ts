b31e87fa51c6b618188987b8d9cf4bdf691047cf
MrMysterius <official.mrmysterius@gmail.com>
2021-11-03 02:09:34 +0100

    Added the rest of the api functionality

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { PORT } from "./config";
 import express from "express";
+import { getRecordID } from "./getRecordID";
 import { getZoneID } from "./getZoneID";
+import { updateRecord } from "./updateRecord";
 
 const app = express();
 
 app.get("/dyndns", async (req, res) => {
-  const zone_id = await getZoneID(req.query.token as string, req.query.zone as string);
+  const zone_id = await getZoneID(req.query.token as string, req.query.zone as string).catch((err) => console.log(err));
+  const record_id = await getRecordID(req.query.token as string, zone_id as string, `${req.query.record}.${req.query.zone}`).catch((err) => console.log(err));
+  await updateRecord(req.query.token as string, zone_id as string, record_id as string, req.query.ip4 as string, `${req.query.record}.${req.query.zone}`).catch(
+    (err) => console.log(err)
+  );
   console.log(zone_id);
 
   return res.status(200).send("200: OK");
