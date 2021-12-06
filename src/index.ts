9f1a7e95fc7eb7215fb7872665b27a7b5c412ab6
MrMysterius <official.mrmysterius@gmail.com>
2021-12-07 00:40:36 +0100

    Added some console logs

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,15 @@ import { updateRecord } from "./updateRecord";
 const app = express();
 
 app.get("/dyndns", async (req, res) => {
+  let date = new Date();
+  console.log(date.toISOString(), req.query);
   const zone_id = await getZoneID(req.query.token as string, req.query.zone as string).catch((err) => console.log(err));
   const record_id = await getRecordID(req.query.token as string, zone_id as string, `${req.query.record}.${req.query.zone}`).catch((err) => console.log(err));
   await updateRecord(req.query.token as string, zone_id as string, record_id as string, req.query.ip4 as string, `${req.query.record}.${req.query.zone}`).catch(
     (err) => console.log(err)
   );
+  date = new Date();
+  console.log(date.toISOString(), req.query);
   console.log(zone_id);
 
   return res.status(200).send("200: OK");
